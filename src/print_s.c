624942fcdf5412ac4c95322ffd4aa98d95f304c0
Truman Chan <tchan@student.42.us.org>
2017-06-04 12:53:57 -0700

    wstr

--- src/print_s.c
+++ src/print_s.c
@@ -66,10 +66,9 @@ void	print_wstring(t_params *params, va_list var_list)
 	wchar_t *wstr;
 		
 	wstr = va_arg(var_list, wchar_t*);
+
 	while(*wstr)
-	{
-		print_wchar(params, var_list, 1, *wstr);
-		wstr++;
-	}
+		print_wchar(params, var_list, 1, *wstr++);
+	
 	params->printed = 1;
 }
